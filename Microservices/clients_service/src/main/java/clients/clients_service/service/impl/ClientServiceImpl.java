5c7e37bc239f5d7330b5e4a22b28c2229fec826d
Oumaima Boughda <oumaima.boughdad@etu.uae.ac.ma>
2024-12-25 22:27:47 +0100

    oumaima/branch

--- Microservices/clients_service/src/main/java/clients/clients_service/service/impl/ClientServiceImpl.java
+++ Microservices/clients_service/src/main/java/clients/clients_service/service/impl/ClientServiceImpl.java
@@ -2,9 +2,11 @@ package clients.clients_service.service.impl;
 
 import clients.clients_service.entity.Client;
 import clients.clients_service.entity.Employee;
+import clients.clients_service.events.ClientPlacedEvent;
 import clients.clients_service.repository.ClientRepository;
 import clients.clients_service.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,39 +16,27 @@ import java.util.Optional;
 @Service
 public class ClientServiceImpl implements ClientService {
 
+    //inject kafka service
+    private final KafkaTemplate<String,ClientPlacedEvent> kafkaTemplate;
+
     @Autowired
     private RestTemplate restTemplate;
 
     private final ClientRepository clientRepository;
 
-    public ClientServiceImpl(ClientRepository clientRepository) {
+    public ClientServiceImpl(ClientRepository clientRepository, KafkaTemplate kafkaTemplate) {
+
         this.clientRepository = clientRepository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    /*
-    Posts posts = restTemplate.getForObject(
-                "http://SERVICE_POST/post/2"
-                ,Posts.class);
-    * */
+
     @Override
     public Client createClient(Client client) {
         //Employee emp =restTemplate.getForObject("http://localhost:8080/api/employees/1", Employee.class);
-
-        //client.setEmployee(emp);
-
-        try {
-            Employee emp = restTemplate.getForObject("http://localhost:8081/api/employees/1", Employee.class);
-
-            if (emp == null) {
-                throw new RuntimeException("Employee not found or service unavailable");
-            }
-
-            client.setEmployee(emp);
-            return clientRepository.save(client);
-        } catch (Exception ex) {
-            throw new RuntimeException("Failed to retrieve employee: " + ex.getMessage());
-        }
-       // return clientRepository.save(client);
+        clientRepository.save(client);
+        kafkaTemplate.send("notificationTopic", new ClientPlacedEvent(client.getIdClient()));
+        return  client;
     }
 
     @Override
