50caa2af1f1705de8da6583313383344707e1c6b
Oumaima Boughda <oumaima.boughdad@etu.uae.ac.ma>
2025-01-16 16:10:30 +0000

    jenkins

--- Microservices/clients_service/src/test/java/clients/clients_service/service/impl/ClientServiceImplTest.java
+++ Microservices/clients_service/src/test/java/clients/clients_service/service/impl/ClientServiceImplTest.java
@@ -1,15 +1,16 @@
+package clients.clients_service.service.impl;
+
 import clients.clients_service.entity.Client;
 import clients.clients_service.repository.ClientRepository;
-import clients.clients_service.service.impl.ClientServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class) // Enable Mockito for this test class
 class ClientServiceImplTest {
@@ -32,21 +33,21 @@ class ClientServiceImplTest {
         client.setEmail("john.doe@example.com");
 
         // Mock the behavior of clientRepository.save()
-        when(clientRepository.save(client)).thenReturn(client);
+        Mockito.when(clientRepository.save(client)).thenReturn(client);
 
         // Act
         Client createdClient = clientService.createClient(client);
 
         // Assert
-        assertNotNull(createdClient, "Created client should not be null");
-        assertEquals("John", createdClient.getFirstName(), "First name should match");
-        assertEquals("Doe", createdClient.getLastName(), "Last name should match");
-        assertEquals("john.doe@example.com", createdClient.getEmail(), "Email should match");
+        Assertions.assertNotNull(createdClient, "Created client should not be null");
+        Assertions.assertEquals("John", createdClient.getFirstName(), "First name should match");
+        Assertions.assertEquals("Doe", createdClient.getLastName(), "Last name should match");
+        Assertions.assertEquals("john.doe@example.com", createdClient.getEmail(), "Email should match");
 
         // Verify that clientRepository.save() was called once
-        verify(clientRepository, times(1)).save(client);
+        Mockito.verify(clientRepository, Mockito.times(1)).save(client);
 
         // Verify that sendClientEvent() was called with the correct arguments
-        verify(kafkaTemplate, times(1)).send(eq("client-events"), eq(client.getIdClient().toString()), anyString());
+        Mockito.verify(kafkaTemplate, Mockito.times(1)).send(ArgumentMatchers.eq("client-events"), ArgumentMatchers.eq(client.getIdClient().toString()), ArgumentMatchers.anyString());
     }
 }
