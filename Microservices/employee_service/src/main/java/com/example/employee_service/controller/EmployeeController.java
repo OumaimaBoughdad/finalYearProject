ba1047050d3fa569686d4fd48f26bbb6024131d9
Oumaima Boughda <oumaima.boughdad@etu.uae.ac.ma>
2025-01-18 02:57:03 +0000

    oumaima/jenkins

--- Microservices/employee_service/src/main/java/com/example/employee_service/controller/EmployeeController.java
+++ Microservices/employee_service/src/main/java/com/example/employee_service/controller/EmployeeController.java
@@ -8,7 +8,6 @@ import com.example.employee_service.service.EmployeeService;
 import com.example.employee_service.service.EmployeeServiceImpl;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,13 +35,6 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    // Create a new employee
-//    @PostMapping
-//    public ResponseEntity<EmployeeDTO> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
-//        EmployeeDTO createdEmployee = employeeService.createEmployee(employeeDTO);
-//
-//        return ResponseEntity.ok(createdEmployee);
-//    }
 
     // Get all employees
     @GetMapping
